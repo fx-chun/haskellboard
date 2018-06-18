80428d79fe4dc40bfab1f53859c461f006aa5111
Andy Chun @noneucat <andy@lolc.at>
2018-06-17 22:47:39 -0700

    Added Yampa dependencies and added basic template

--- Main.hs
+++ Main.hs
@@ -1,4 +1,39 @@
-module Main where
+-- based on example in https://wiki.haskell.org/Yampa/reactimate
+
+import Control.Monad
+import Data.IORef
+import Data.Time.Clock
+import FRP.Yampa
+
+data Inputs = Inputs { }
+
+data Outputs = Outputs { }
+
+outputs :: SF Inputs Outputs
+outputs = arr (\_ -> Outputs {} ) -- todo
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  t <- getCurrentTime
+  timeRef <- newIORef t
+  reactimate initialize (sense timeRef) actuate outputs
+
+initialize :: IO Inputs
+initialize = do
+  putStrLn "Hello... wait for it..."
+  return (Inputs {})
+ 
+actuate :: Bool -> Outputs -> IO Bool
+actuate _ x = return False
+
+sense :: IORef UTCTime -> Bool -> IO (Double, Maybe Inputs)
+sense timeRef _ = do
+  now      <- getCurrentTime
+  lastTime <- readIORef timeRef
+  writeIORef timeRef now
+  let dt = now `diffUTCTime` lastTime
+
+  -- todo do input gathering here
+  inputs <- undefined
+
+  return (realToFrac dt, Just inputs)
