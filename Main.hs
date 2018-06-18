379aa3a68473f5e7bdd2846ae79477b9f6c09721
Andy Chun @noneucat <andy@lolc.at>
2018-06-18 12:51:00 -0700

    Added SDL test code

--- Main.hs
+++ Main.hs
@@ -4,27 +4,62 @@ import Control.Monad
 import Data.IORef
 import Data.Time.Clock
 import FRP.Yampa
+import SDL.Input.Joystick
 
-data Inputs = Inputs { }
+data Inputs = Inputs { 
+  iThrottle       :: Throttle,
 
-data Outputs = Outputs { }
+  iBUp            :: Event (),
+  iBDown          :: Event (),
+  iBLeft          :: Event (),
+  iBRight         :: Event (),
+  iBTrigger       :: Event (),
+  iBShoulder      :: Event (),
 
-outputs :: SF Inputs Outputs
-outputs = arr (\_ -> Outputs {} ) -- todo
+  iDebug          :: Int
+}
+
+defaultInputs = Inputs {
+  iThrottle = 0.0,
+
+  iBUp = NoEvent,
+  iBDown = NoEvent,
+  iBLeft = NoEvent,
+  iBRight = NoEvent,
+  iBTrigger = NoEvent,
+  iBShoulder = NoEvent,
+
+  iDebug = 0
+}
+
+data Outputs = Outputs { 
+  oPrintBuffer :: [Char],
+  oPWMOutput :: DutyCycle
+}
+
+defaultOutputs = Outputs {
+  oPrintBuffer = [],
+  oPWMOutput = 0.0
+}
+
+type Throttle = Double
+type DutyCycle = Double
 
 main :: IO ()
 main = do
   t <- getCurrentTime
   timeRef <- newIORef t
-  reactimate initialize (sense timeRef) actuate outputs
+  reactimate initialize (sense timeRef) actuate outputsSignal
 
 initialize :: IO Inputs
 initialize = do
   putStrLn "Hello... wait for it..."
-  return (Inputs {})
- 
-actuate :: Bool -> Outputs -> IO Bool
-actuate _ x = return False
+
+  j <- numJoysticks
+
+  return (defaultInputs {
+    iDebug = fromIntegral j 
+  })
 
 sense :: IORef UTCTime -> Bool -> IO (Double, Maybe Inputs)
 sense timeRef _ = do
@@ -33,7 +68,21 @@ sense timeRef _ = do
   writeIORef timeRef now
   let dt = now `diffUTCTime` lastTime
 
-  -- todo do input gathering here
-  inputs <- undefined
+  inputs <- gatherInputs
 
   return (realToFrac dt, Just inputs)
+
+gatherInputs :: IO Inputs
+gatherInputs = return (defaultInputs)
+
+actuate :: Bool -> Outputs -> IO Bool
+actuate _ outputs = do
+  if null $ oPrintBuffer outputs
+    then return ()
+    else putStrLn $ oPrintBuffer outputs
+  return False
+
+outputsSignal :: SF Inputs Outputs
+outputsSignal = arr (\i -> defaultOutputs {
+  oPrintBuffer = show $ iDebug i
+}) -- todo
