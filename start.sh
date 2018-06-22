9509363ec720a23fdd1357a6c8eb46601a2b2d38
Andy Chun @noneucat <andy@lolc.at>
2018-06-21 21:31:16 -0700

    Fix syntax error in bash file

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@
 while true; do
     sleep 1;
     if [ ! -e /dev/input/event0 ]; then
-        { echo -n; echo -e 'disconnect 60:6B:FF:67:18:57\n'; sleep 10; 'connect 60:6B:FF:67:18:57\n'; sleep 5; echo -e 'quit\n'; } | bluetoothctl; 
+        { echo -n; echo -e 'disconnect 60:6B:FF:67:18:57\n'; sleep 10; echo -e 'connect 60:6B:FF:67:18:57\n'; sleep 5; echo -e 'quit\n'; } | bluetoothctl; 
     fi
 done
