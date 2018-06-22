8b07181d956b8037423e1a9c1c1c5d871ee418f3
Andy Chun @noneucat <andy@lolc.at>
2018-06-21 18:10:21 -0700

    Don't attempt to connect to the controller if we are already connected

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 ./dist/build/haskellboard/haskellboard &
 
 while true; do
-    sleep 1;
-    { echo -n; echo -e 'power on\n'; sleep 1; echo -e 'connect \t\n'; sleep 1; echo -e 'quit\n'; } | bluetoothctl; 
+    sleep 2;
+    if [ ! -f /dev/input/event0 ]; then
+        { echo -n; echo -e 'power on\n'; sleep 1; echo -e 'connect \t\n'; sleep 1; echo -e 'quit\n'; } | bluetoothctl; 
+    fi
 done
