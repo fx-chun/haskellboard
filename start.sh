f3bb9e8a7ef1f33e2c3f988403cd788e50fb52ac
Andy Chun @noneucat <andy@lolc.at>
2018-06-21 21:27:42 -0700

    Tweak script to attempt disconnecting before trying to (re)connect

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 ./dist/build/haskellboard/haskellboard &
 
 while true; do
-    sleep 2;
+    sleep 1;
     if [ ! -e /dev/input/event0 ]; then
-        { echo -n; echo -e 'power on\n'; sleep 1; echo -e 'connect \t\n'; sleep 1; echo -e 'quit\n'; } | bluetoothctl; 
+        { echo -n; echo -e 'disconnect 60:6B:FF:67:18:57\n'; sleep 10; 'connect 60:6B:FF:67:18:57\n'; sleep 5; echo -e 'quit\n'; } | bluetoothctl; 
     fi
 done
